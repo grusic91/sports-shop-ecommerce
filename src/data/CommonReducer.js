b1d16ce40fdbff4e29ba44fde55c904f47015283
Gruzic <28716217+grusic91@users.noreply.github.com>
2020-06-04 16:41:19 +0200

    Adds features to app and set view

---
+++ src/data/CommonReducer.js
@@ -0,0 +1,15 @@
+export const CommonReducer = (...reducers) => (storeData, action) => {
+    for ( let i = 0; i < reducers.length; i++) {
+        let newStore = reducers[i](storeData, action);
+        if ( newStore !== storeData) {
+            return newStore;
+        }
+    }
+    return storeData;
+}
+
+/* 
+    function combines multiple reducers into a single function 
+    and asks each of them to handle actions. Reducers return new object when they modify the contents of
+    the data store
+*/
